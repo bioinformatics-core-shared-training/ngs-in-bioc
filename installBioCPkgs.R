417401c51321a811b9c37de871a50617afbf9f52
markdunning <mark.dunning@gmail.com>
2015-05-26 09:55:24 +0100

    add ChIPpeakAnno to list of packages

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -4,7 +4,7 @@ options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("Biostrings", "ShortRead", "DESeq", "edgeR","biomaRt", "BSgenome",
         "BSgenome.Dmelanogaster.UCSC.dm6", "org.Dm.eg.db",
         "TxDb.Dmelanogaster.UCSC.dm3.ensGene", "pasillaBamSubset", "pasilla",
-        "rtracklayer", "ggbio", "vsn","gplots","RColorBrewer","chipseq","htSeqTools","limma","NBPSeq","tweeDEseqCountData","org.Hs.eg.db","Rcade", "exomeCopy","CNAnorm", "ChIPQC","TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19"))
+        "rtracklayer", "ggbio", "vsn","gplots","RColorBrewer","chipseq","htSeqTools","limma","NBPSeq","tweeDEseqCountData","org.Hs.eg.db","Rcade", "exomeCopy","CNAnorm", "ChIPQC","TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19","ChIPpeakAnno"))
 
 ##Also need to install spp_1.11.tar.gz which requires the 'boost' library.
 
